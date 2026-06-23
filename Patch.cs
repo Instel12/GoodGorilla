49076e50a8ebdd6e6bde21d9377067289ac07c1d
Lance <135399235+Instel12@users.noreply.github.com>
2026-06-23 11:37:19 -0400

    this *should* work

---
+++ Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace GoodGorilla
+{
+    [HarmonyPatch(typeof(VRRig), nameof(VRRig.NormalizeName))]
+    class Patch
+    {
+        static void Postfix(bool doIt, string text, ref string __result)
+        {
+            if (text.Length > 12)
+            {
+                __result = $"<color=blue>{text}</color>";
+            }
+            if (__result == "BADGORILLA")
+            {
+                __result = $"<color=red>{text}</color>";
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace GoodGorilla
+{
+    [BepInPlugin("instel.goodgorilla", "GoodGorilla", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony = new Harmony("instel.goodgorilla");
+
+        public void Awake()
+        {
+            harmony = new Harmony("instel.goodgorilla");
+            harmony.PatchAll();
+        }
+
+        public void OnDisable() => harmony.UnpatchSelf();
+    }
+}
