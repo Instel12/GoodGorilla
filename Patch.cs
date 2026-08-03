f465b67eb1d801e43dad41167953028623dc984b
Lance <135399235+Instel12@users.noreply.github.com>
2026-08-03 15:08:08 -0400

    better kinda
    ive NEVER used the extended desc 💀
    the original tried to change colors if it was originally blocked but that made it breaks so now you gotta guess if it was blocked originally but at least it works

--- Patch.cs
+++ Patch.cs
@@ -1,20 +1,14 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 
 namespace GoodGorilla
 {
     [HarmonyPatch(typeof(VRRig), nameof(VRRig.NormalizeName))]
     class Patch
     {
-        static void Postfix(bool doIt, string text, ref string __result)
+        static bool Prefix(bool doIt, string text, ref string __result)
         {
-            if (text.Length > 12)
-            {
-                __result = $"<color=blue>{text}</color>";
-            }
-            if (__result == "BADGORILLA")
-            {
-                __result = $"<color=red>{text}</color>";
-            }
+            __result = text;
+            return false;
         }
     }
 }
